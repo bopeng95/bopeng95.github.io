381fed62638852a842068239031ff9706be2fddb
Bo Peng <bopeng@bos-macbook-pro.attlocal.net>
2017-06-24 16:03:01 -0700

    organized html js layout

--- styles/custom.js
+++ styles/custom.js
@@ -1,55 +1,41 @@
-    $('body').scrollspy({ target: '#navbar-example' })    
+$(document).ready(function() {
 
-    $(window).scroll(function() {
-    if ($(this).scrollTop()) {
-        $("#up").fadeIn();
-    } else {
-        $("#up").fadeOut();
-    }
-    });
-    $(document).ready(function() {
+        $('body').scrollspy({ target: '#navbar-example' });
+        
+        $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $("#up").fadeIn();
+        } else {
+            $("#up").fadeOut();
+        }
+        });
+        
         var away = false;
         $("#up").click(function() {
             $("html, body").animate({
                 scrollTop: 0
-            }, 500);
+            }, 700);
         });    
-    });
 
-    $("#sec").click(function() {
-        $("html, body").animate({
-            scrollTop: $("#2").offset().top
-        }, 500);
-    });
-    $("#first").click(function() {
-        $("html, body").animate({
-            scrollTop: $("#1").offset().top
-        }, 500);
-    });
-    $("#third").click(function() {
-        $("html, body").animate({
-            scrollTop: $("#3").offset().top
-        }, 500);
-    });
-    $("#fourth").click(function() {
-        $("html, body").animate({
-            scrollTop: $("#4").offset().top
-        }, 500);
-    });
-
-    $(window).scroll(function() {
-    if ($(this).scrollTop()) {
-        $("#up").fadeIn();
-    } else {
-        $("#up").fadeOut();
-    }
-    });
-
-    $(document).ready(function() {
-        var away = false;
-        $("#up").click(function() {
+        $("#sec").click(function() {
             $("html, body").animate({
-                scrollTop: 0
-            }, 500);
-        });    
-    });
+                scrollTop: $("#2").offset().top
+            }, 700);
+        });
+        $("#first").click(function() {
+            $("html, body").animate({
+                scrollTop: $("#1").offset().top
+            }, 700);
+        });
+        $("#third").click(function() {
+            $("html, body").animate({
+                scrollTop: $("#3").offset().top
+            }, 700);
+        });
+        $("#fourth").click(function() {
+            $("html, body").animate({
+                scrollTop: $("#4").offset().top
+            }, 700);
+        });
+
+});

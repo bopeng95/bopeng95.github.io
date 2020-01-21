3ac44a09c72fc7d20f4483d86d7a46263ae1c2e5
Bo Peng <29678468+bopeng95@users.noreply.github.com>
2020-01-21 11:59:28 -0800

    update

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import Fade from 'components/Fade';
 import Main from 'components/Main';
-import Image from 'components/Image';
+// import Image from 'components/Image';
 import Text from 'components/Text';
 import CopyrightIcon from 'components/Icons/CopyrightIcon';
 
-import Me from 'assets/me.jpg';
+// import Me from 'assets/me.jpg';
 import Resume from 'assets/resume.pdf';
 
 import Details from './components/Details';
@@ -21,7 +21,7 @@ const App = () => {
         <Details title="hello, welcome to my amazing website" />
         <Contact Resume={Resume} />
       </Fade>
-      <Image src={Me} width="100%" height="200px" asBackground opacity={0.8} />
+      {/* <Image src={Me} width="100%" height="200px" asBackground opacity={0.8} /> */}
 
       <Section direction="row" vertical="center" gutterTop={150}>
         <Text type="subscript" color="gray">
